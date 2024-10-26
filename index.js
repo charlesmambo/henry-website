a902ef6c7b75f3cc5db5c00aa56e80d7e0bd466f
Charles-M <chylah11st@gmail.com>
2024-10-26 11:43:40 +0200

    complete website

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const serviceImg = document.querySelector('.service-img');
+const openBtn = document.getElementById('hamburger');
+const dropDown = document.getElementById('drop-down');
+const closeBtn = document.getElementById('close-btn');
+const links = document.querySelectorAll('#drop-down a'); // Select all links inside the dropdown
 
-let scrollAmount = 0;
-
-nextBtn.addEventListener('click', () => {
-    serviceImg.scrollBy({ left: 200, behavior: 'smooth' }); // Scroll to the right
+openBtn.addEventListener('click', () => {
+    dropDown.style.display = 'block';
 });
 
-prevBtn.addEventListener('click', () => {
-    serviceImg.scrollBy({ left: -200, behavior: 'smooth' }); // Scroll to the left
+closeBtn.addEventListener('click', () => {
+    dropDown.style.display = 'none';
 });
+
+// Add event listener to each link to close dropdown on click
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        dropDown.style.display = 'none';
+    });
+});
+
+
+document.getElementById('see-all-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    const hiddenCard = document.getElementById('hidden-card');
+    
+    // Add class to show card with animation
+    hiddenCard.classList.add('show');
+    
+    setTimeout(() => {
+        hiddenCard.style.display = 'flex';
+    }, 3000); 
+    
+    this.style.display = 'none';
+});
+
+document.getElementById('see-all-services').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    const hiddenCards = document.querySelectorAll('#hidden-content .card.hide');
+
+    hiddenCards.forEach(card => {
+        card.classList.remove('hide'); // Remove the hide class
+    });
+
+    // Hide the "See All" button after cards are displayed
+    this.style.display = 'none';
+});
+
+
+
